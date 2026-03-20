57ebc480c5fdcd88298aad609b55abcc71ad9164
Michael Ashby <michael.ashby@hisawyer.com>
2026-03-20 14:43:25 -0500

    add browse widget scenario

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, between, events
 from scenarios.visit_widget import VisitWidgetScenario
+from scenarios.browse_widget import BrowseWidgetScenario
 from scenarios.place_order import PlaceOrderScenario
 from utils.auth import login
 import os
@@ -16,22 +17,6 @@ class RailsUser(HttpUser):
         else:
             users_module = importlib.import_module("utils.users")
 
-        # Set realistic browser headers
-        # self.client.headers.update({
-        #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        #     'Accept': 'application/json, text/plain, */*',
-        #     'Accept-Language': 'en-US,en;q=0.9',
-        #     'Accept-Encoding': 'gzip, deflate, br',
-        #     'Referer': 'https://www.hisawyer.com/',
-        #     'Origin': 'https://www.hisawyer.com',
-        #     'Sec-Fetch-Dest': 'empty',
-        #     'Sec-Fetch-Mode': 'cors',
-        #     'Sec-Fetch-Site': 'same-origin',
-        #     'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-        #     'sec-ch-ua-mobile': '?0',
-        #     'sec-ch-ua-platform': '"macOS"'
-        # })
-
         self.user = users_module.get_random_user()
 
         scenario = self.environment.parsed_options.scenario
@@ -39,6 +24,8 @@ class RailsUser(HttpUser):
             self.tasks = ['view_explore']
         elif scenario == "visit_widget":
             self.tasks = [VisitWidgetScenario]
+        elif scenario == "browse_widget":
+            self.tasks = [BrowseWidgetScenario]
         elif scenario == "place_order":
             self.tasks = [PlaceOrderScenario]
         elif scenario == "rush":
@@ -51,6 +38,6 @@ class RailsUser(HttpUser):
 @events.init_command_line_parser.add_listener
 def custom_args(parser):
     parser.add_argument("--scenario", choices=["place_order",
-                        "visit_widget", "rush"], default="place_order", help="Scenario")
+                        "visit_widget", "rush", "browse_widget"], default="place_order", help="Scenario")
     parser.add_argument("--slug", is_required=True, default="pretend-school")
-    parser.add_argument("--booking_fee_id", is_required=True, default="306")
+    parser.add_argument("--booking_fee_id", is_required=True, default="154") # use 306 for production
---
+++ scenarios/browse_widget.py
@@ -0,0 +1,126 @@
+from locust import SequentialTaskSet, task
+from datetime import datetime
+import os
+import json
+import random
+import time
+from urllib.parse import urlencode
+from utils.auth import extract_csrf_token, login
+
+
+class BrowseWidgetScenario(SequentialTaskSet):
+    def on_start(self):
+        self.slug = self.user.environment.parsed_options.slug
+        self.iteration_count = 0
+
+    @task
+    def browse_widget(self):
+        user = self.user.user
+        time.sleep(random.uniform(1, 10))
+        csrf_token = login(self.client, user)
+
+        time.sleep(random.uniform(1, 10))
+
+        now = datetime.now()
+        html_headers = {"Accept": "text/html"}
+        json_headers = {"Accept": "application/json"}
+
+        number_of_ages = random.choice(range(1, 10))
+        age_ranges = random.sample(range(11, 31), k=number_of_ages)
+        month = random.randint(1, 12)
+        year = now.year
+        time_picker_min = random.randint(21600, 40000)
+        time_picker_max = random.randint(75000, 90000)
+        adults_only = random.choice([0, 1])
+
+        self.client.get(  # Load widget calendar view
+            f"/{self.slug}/schedules/widget_calendar",
+            headers=html_headers
+        )
+
+        self.client.get(  # Load widget calendar view with time filters
+            self._build_widget_calendar_filter_url({
+                "month": month,
+                "year": year,
+                "time_picker_max": time_picker_max,
+                "time_picker_min": time_picker_min
+            }),
+            headers=html_headers
+        )
+
+        time.sleep(random.uniform(1, 10))
+
+        self.client.get(  # Clear the filters
+            f"/{self.slug}/schedules/widget_calendar",
+            headers=html_headers
+        )
+
+        time.sleep(random.uniform(1, 10))
+
+        self.client.get(  # Load widget calendar view with age filters & time filters
+            self._build_widget_calendar_filter_url({
+                "month": month,
+                "year": year,
+                "time_picker_max": time_picker_max,
+                "time_picker_min": time_picker_min,
+                "age_ranges": age_ranges,
+                "adults_only": adults_only
+            }),
+            headers=html_headers
+        )
+
+        time.sleep(random.uniform(1, 10))
+
+        for _ in range(25):
+            self.client.get(  # Visit widget list view
+                f"/{self.slug}/schedules",
+                headers=html_headers
+            )
+
+            scheduled_activities = self.client.get(  # Load data for widget list view
+                f"/api/v1/widget/scheduled_activities?slug={self.slug}&page=1",
+                headers=json_headers
+            )
+
+            print("scheduled activities:")
+            print(scheduled_activities)
+
+            data = json.loads(scheduled_activities.text) # TODO drawing an error here: JSONDecodeError("Expecting value", s, err.value) from Nonejson.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+            results = data.get("data", {}).get("results", [])
+            asg_id = results[0]["id"] if results else None
+
+            time.sleep(random.uniform(1, 10))
+
+            self.client.get(  # Load widget list view w/ filters
+                self._build_widget_filter_url({
+                    "time_picker_max": time_picker_max,
+                    "time_picker_min": time_picker_min,
+                    "age_ranges": age_ranges
+                }),
+                headers=html_headers
+            )
+
+            time.sleep(random.uniform(1, 10))
+
+            self.client.get(  # Visit widget list view
+                f"/{self.slug}/schedules",
+                headers=html_headers
+            )
+
+            if asg_id:
+                print(f"/{self.slug}/schedules/activity-set/{asg_id}?source=semesters")
+                self.client.get(  # Visit PDP from widget
+                    f"/{self.slug}/schedules/activity-set/{asg_id}?source=semesters"
+                )
+
+            time.sleep(random.uniform(1, 10))
+
+            self.iteration_count += 1
+
+        return
+
+    def _build_widget_calendar_filter_url(self, params):
+        return f"/{self.slug}/schedules/widget_calendar?{urlencode(params, doseq=True)}"
+
+    def _build_widget_filter_url(self, params):
+        return f"/{self.slug}/schedules?{urlencode(params, doseq=True)}"
