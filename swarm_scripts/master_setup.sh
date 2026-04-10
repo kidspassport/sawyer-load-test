354c3af9e8ab5bc68472088ffdf901e7ac276812
Michael Ashby <michael.ashby@hisawyer.com>
2026-04-10 10:13:28 -0500

    use venv

--- swarm_scripts/master_setup.sh
+++ swarm_scripts/master_setup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Update system
 apt-get update
-apt-get install -y python3 python3-pip git
+apt-get install -y python3 python3-pip python3-venv git
 
 # Create locust user
 useradd -m -s /bin/bash locust
@@ -13,20 +13,12 @@ cd /home/locust
 git clone https://github.com/kidspassport/sawyer-load-test.git
 cd sawyer-load-test
 
-# Remove problematic system packages that conflict with pip
-apt-get remove -y python3-blinker python3-zope.interface python3-urllib3 python3-requests || true
+# Create virtual environment
+python3 -m venv /home/locust/venv
 
-# Install Python dependencies - upgrade pip first
-pip3 install --upgrade pip setuptools wheel
-
-# Force install gevent and its dependencies first
-pip3 install --force-reinstall --no-cache-dir zope.event zope.interface greenlet gevent
-
-# Force reinstall urllib3 and requests to ensure correct versions
-pip3 install --force-reinstall --no-cache-dir urllib3 requests
-
-# Now install other requirements
-pip3 install -r requirements.txt
+# Install Python dependencies in virtual environment
+/home/locust/venv/bin/pip install --upgrade pip setuptools wheel
+/home/locust/venv/bin/pip install -r requirements.txt
 
 # Create systemd service for master
 cat > /etc/systemd/system/locust-master.service <<EOF
@@ -38,10 +30,10 @@ After=network.target
 Type=simple
 User=locust
 WorkingDirectory=/home/locust/sawyer-load-test
-Environment="PATH=/usr/local/bin:/usr/bin:/bin"
+Environment="PATH=/home/locust/venv/bin:/usr/local/bin:/usr/bin:/bin"
 Environment="PYTHONUNBUFFERED=1"
 Environment="LOAD_TEST_TOKEN=\${load_test_token}"
-ExecStart=/usr/local/bin/locust \\
+ExecStart=/home/locust/venv/bin/locust \\
   --master \
   --master-bind-port=5557 \
   --web-port=8089 \
